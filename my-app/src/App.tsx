3c4c52c4f7a6e30dc96f37582ce717920aeee205
Shrey Tailor <shreym.tailor@gmail.com>
2020-07-23 12:25:50 +1200

    another test.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   console.log(process.env.NODE_ENV.TITLE)
   return (
-    <p>Testing the develop branch.</p>
+    <p>Testing the develop branch :)</p>
   );
 }
 
